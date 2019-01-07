a291b86009803f3de83511ca3f0a180a18f4e846
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-07 13:48:38 -0700

    HeaderContainer populated and styled

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 function Header() {
   return (
     <div className="header-container">
       <ImageThumbnail />
+      <HeaderTitle />
     </div>
   );
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+  return (
+    <div className="header-title">
+      <div className="header-name">
+        Lambda School
+      </div>
+      <div className="header-handle">
+        @LambdaSchool
+      </div>
+      <div className="header-timestamp">
+        • 26 jan
+      </div>
+    </div>
+  );
+}
+
+export default HeaderTitle;
