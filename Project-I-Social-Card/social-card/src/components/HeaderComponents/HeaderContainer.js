d5483784d9a8112a214241fddf64568a1fe7f714
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-07 13:22:11 -0700

    HeaderContainer set up, ImageThumbnail complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <ImageThumbnail />
+    </div>
+  );
+}
+
+export default Header;
