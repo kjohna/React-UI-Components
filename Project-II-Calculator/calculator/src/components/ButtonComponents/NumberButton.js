5b98aa090dcf995a89e520405dc95f1e7890cff7
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-08 14:50:32 -0700

    NumberButton written functionally

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+  const {buttonStyle, text} = props.button;
+  
+  return (
+      <button className={buttonStyle}>{text}</button>
+    );
+  }
+
+export default NumberButton;
