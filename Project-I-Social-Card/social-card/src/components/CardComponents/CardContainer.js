3716ebf7155f5ab4fa12f5cfb4fb9084fc1100df
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-07 22:22:57 -0700

    CardContent complete, styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 function CardContainer() {
   return (
     <div className="card-container">
       <CardBanner />
+      <CardContent />
     </div>
   );
 }
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+  return (
+    <div className="card-content">
+      <h3>Get started  with React</h3>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views for each state in your application.
+      </p>
+      <a href="#" className="card-content-link">
+        reactjs.org
+      </a>
+    </div>
+  );
+}
+
+export default CardContent;
