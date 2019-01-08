671b183e1b77d5f4485bffa1db4f2cb49393cd18
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-08 15:26:39 -0700

    button styling getting close

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -9,26 +9,26 @@ const App = () => {
     <div className="calculator-container">
       <Display />
       <div className="buttons-row-1">
-        <ActionButton buttonStyle="button-white" text="clear" />
+        <ActionButton buttonStyle="button-white button-wide" text="clear" />
         <NumberButton buttonStyle="button-red" text="÷" />
       </div>
       <div className="buttons-row-2"></div>
-        <NumberButton buttonStyle="button-red" text="7" />
-        <NumberButton buttonStyle="button-red" text="8" />
-        <NumberButton buttonStyle="button-red" text="9" />
+        <NumberButton buttonStyle="button-white" text="7" />
+        <NumberButton buttonStyle="button-white" text="8" />
+        <NumberButton buttonStyle="button-white" text="9" />
         <NumberButton buttonStyle="button-red" text="x" />
       <div className="buttons-row-3"></div>
-        <NumberButton buttonStyle="button-red" text="4" />
-        <NumberButton buttonStyle="button-red" text="5" />
-        <NumberButton buttonStyle="button-red" text="6" />
+        <NumberButton buttonStyle="button-white" text="4" />
+        <NumberButton buttonStyle="button-white" text="5" />
+        <NumberButton buttonStyle="button-white" text="6" />
         <NumberButton buttonStyle="button-red" text="-" />
       <div className="buttons-row-4"></div>
-        <NumberButton buttonStyle="button-red" text="1" />
-        <NumberButton buttonStyle="button-red" text="2" />
-        <NumberButton buttonStyle="button-red" text="3" />
+        <NumberButton buttonStyle="button-white" text="1" />
+        <NumberButton buttonStyle="button-white" text="2" />
+        <NumberButton buttonStyle="button-white" text="3" />
         <NumberButton buttonStyle="button-red" text="+" />
       <div className="buttons-row-5"></div>
-        <ActionButton buttonStyle="button-white" text="0" />
+        <ActionButton buttonStyle="button-white button-wide" text="0" />
         <NumberButton buttonStyle="button-red" text="=" />
     </div>
   );
