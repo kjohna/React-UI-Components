7bab994d8c3623556715c91f37404429c2dce526
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-08 14:35:59 -0700

    display built, styled

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Display.css';
+
+class Display extends Component {
+
+  render () {
+
+    return (
+      <div className="display-container">
+          0
+      </div>
+    );
+  }
+}
+
+export default Display;
