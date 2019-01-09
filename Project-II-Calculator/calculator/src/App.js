74b723600aafcf29b1952455c060fff4683b1964
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-09 09:42:23 -0700

    functionality sort of working but buggy

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -17,15 +17,81 @@ class App extends Component {
   clickHandler(btnPressed) {
     console.log("button pressed: ", btnPressed);
 
-    if (typeof btnPressed === 'number') {
+    if (typeof btnPressed === 'number' || btnPressed === 0) {
+      // number was pressed
+      if (this.state.operation === null){
+        // simply update total
+        this.setState((state) => {
+          return {
+            total: (state.total * 10) + btnPressed
+          };
+        });
+      } else {
+        // operation was pressed but hasn't been eval'd
+        if (this.state.total === this.state.buffer) {
+          // operation was just pressed, total still holds previous result (entered or result of an eval)
+          // start updating total with new value
+          this.setState((state) => {
+            return {
+              buffer: state.total,
+              total: btnPressed,
+            };
+          });
+        } else {
+          // operation was pressed previously but still accepting updates to total
+          this.setState((state) => {
+            return {
+              total: (state.total*10) + btnPressed,
+            };
+          });
+        }
+        
+      }      
+    } else if (btnPressed === 'clear') {
       this.setState((state) => {
-        return { total: (state.total * 10) + btnPressed};
+        return {
+          total: 0,
+          buffer: null,
+          operation: null
+        };
       });
-    } else if (btnPressed === "=") {
+    }else if (btnPressed === '=') {
       if (this.state.buffer != null) {
-        console.log("= pressed, do operation");
+        // console.log("= pressed, do operation");
+        // perform operation with buffer and total
+        this.setState((state) => {
+          return {
+            total: eval(`${state.buffer}${state.operation}${state.total}`),
+            buffer: null
+          };
+        });
+      } else {
+        // // perform operation with total and total
+        // this.setState((state) => {
+        //   return {
+        //     total: eval(`${state.total}${state.operation}${state.total}`)
+        //   };
+        // });
       }
-      console.log("= pressed, no operation selected");
+      // console.log("= pressed, no operation selected");
+    } else {
+      // operation was pressed
+      if (this.state.buffer === null) {
+        // nothing in buffer, put current total into buffer
+        this.setState((state) => {
+          return {
+            buffer: state.total,
+            operation: btnPressed
+          };
+        });
+      } else {
+        // something already in buffer, change operation
+        this.setState((state) => {
+          return {
+            operation: btnPressed
+          };
+        });
+      }
     }
   }
 
@@ -41,7 +107,7 @@ class App extends Component {
           <NumberButton buttonStyle="button-white" text="7" onClick={this.clickHandler.bind(this, 7)} />
           <NumberButton buttonStyle="button-white" text="8" onClick={this.clickHandler.bind(this, 8)} />
           <NumberButton buttonStyle="button-white" text="9" onClick={this.clickHandler.bind(this, 9)} />
-          <NumberButton buttonStyle="button-red" text="×" onClick={this.clickHandler.bind(this, "x")} />
+          <NumberButton buttonStyle="button-red" text="×" onClick={this.clickHandler.bind(this, "*")} />
         </div>
         <div className="buttons-row-3">
           <NumberButton buttonStyle="button-white" text="4" onClick={this.clickHandler.bind(this, 4)} />
